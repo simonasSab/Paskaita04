aaa950e5882aeffc4d8b832eff94c90b8a56475f
Simonas Sabaliauskas <sim.sabaliauskas@gmail.com>
2024-05-20 11:08:29 +0300

    Pervadintas projektas Paskaita04 į Uzduotis01. Įkelta baigta Uzduotis02.

---
+++ Uzduotis02/Program.cs
@@ -0,0 +1,29 @@
+﻿using Uzduotis02;
+
+namespace Paskaita04
+{
+    public class Program
+    {
+        // Užduotis 2: Klasės ir Objektai
+        // Sukurkite programą, kuri atliks šias užduotis:
+        // Sukurkite klasę Studentas su šiais laukais: vardas(string), amzius(int), balas(double).
+        // Sukurkite metodą, kuris grąžina studento informaciją kaip string.
+        // Sukurkite kelis studentų objektus ir pridėkite juos į sąrašą.
+        // Išspausdinkite visų studentų informaciją.
+        public static void Main(string[] args)
+        {
+            Studentas studentas1 = new("Amelija", 21, 8.75);
+            Studentas studentas2 = new("Benas", 20, 9.15);
+            Studentas studentas3 = new("Cicinas", 22, 5.15);
+            Studentas studentas4 = new("Daiva", 21, 9.95);
+            Studentas studentas5 = new("Emilija", 19, 8.8);
+
+            List<Studentas> studentai = new() { studentas1, studentas2, studentas3, studentas4, studentas5 };
+
+            foreach (Studentas studentas in studentai)
+            {
+                Console.WriteLine(studentas.ReturnAllAsString());
+            }
+        }
+    }
+}
---
+++ Uzduotis02/Studentas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzduotis02
+{
+    public class Studentas
+    {
+        public string Vardas { get; set; }
+        public int Amzius { get; set; }
+        public double Balas { get; set; }
+
+        public string ReturnAllAsString()
+        {
+            return $"{Vardas} {Amzius} {Balas}";
+        }
+
+        public Studentas(string vardas, int amzius, double balas)
+        {
+            Vardas = vardas;
+            Amzius = amzius;
+            Balas = balas;
+        }
+    }
+}
